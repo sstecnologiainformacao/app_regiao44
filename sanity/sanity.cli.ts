781fd047574e62c6f1299e5c8627a0e043f64679
João Lucas dos Santos <sstecnologiainformacao@gmail.com>
2024-03-09 07:56:55 -0300

    Add sanity para o inicio da criação do backend

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hii6urj5',
+    dataset: 'production'
+  }
+})
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -58,7 +58,7 @@ const HomeScreen = () => {
         <ScrollView
           className="bg-gray-100"
           contentContainerStyle={{
-            paddingBottom: 300,
+            paddingBottom: 250,
           }}
         >
           <Categories />
